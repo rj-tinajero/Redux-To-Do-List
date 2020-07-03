cc1314808f7b4c0caa78ea08816b39503c8be2a4
Richard Tinajero <rj.tinajero92@gmail.com>
2020-07-02 22:57:36 -0700

    add item

--- src/App.js
+++ src/App.js
@@ -1,30 +1,45 @@
-import React, {Component} from 'react';
+import React, {useState} from 'react';
+import {connect} from 'react-redux'
+import {addTodo, changeTodoStatus, deleteFromList} from './redux'
 import './App.css';
 
-class App extends Component{
-  state = {
-    todos : ["this is what I need to do!"],
-  }
-  
-  render() {
-    return(
-      <div className="App-header">
-        <h1>Redux To-Do List</h1>
-        <ul>
-        {this.state.todos.map((item, index) => 
-          <li key={index}>{item}</li>
-        )}
-        </ul>
-        
-        <form>
-          <p>Enter Todo item:</p>
-          <input type="text" placeholder="Type here"/>
-          <input type="submit"/> 
-        </form>
-      </div>
-    )
+function App(props){
+  const [text, setText] = useState("")
+console.log(props, "PROPS TO YAS")
+  const handleChange = (e) => {
+    setText(e.target.value)
+    console.log(text, "<<<<<<<<<")
   }
 
+  const submit = (text) => {
+    props.addTodo(text)
+    // setText("")
+  }
+
+  return(
+    <div className="App-header">
+      <h1>Redux To-Do List</h1>
+      <ul>
+      {props.todos.map((item, index) => 
+        <li key={index}>{item.task}</li>
+      )}
+      </ul>
+      
+      <p>Enter Todo item:</p>
+      <input type="text" placeholder="Type here" onChange={handleChange} />
+      <button onClick={() => submit(text)}>Add Task</button>
+      {/* button instead */}
+
+    </div>
+  )
 }
 
-export default App;
+const mapStateToProps = (state /*, ownProps*/) => {
+  return {
+    todos: state.todos
+  }
+}
+const mapDispatchToProps = { addTodo, changeTodoStatus, deleteFromList }
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
