69e96783819fb92adf11471daecfc3ddefa95b52
Richard Tinajero <rj.tinajero92@gmail.com>
2020-07-27 15:45:01 -0700

    final edits for now

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,35 +1,42 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {connect} from 'react-redux'
 import {addTodo, changeTodoStatus, deleteFromList} from '../redux/actions'
 
 function Todo(props) {
+   const [hover, setHover] = useState(false)
    const item = props.item
    const index = props.index
    return(
-      <li className="row" key={index}>
-         <div className="col my-auto text-center">
+      <li 
+         className="row" 
+         key={index} 
+         onMouseEnter={() => setHover(true)}
+         onMouseLeave={() => setHover(false)}
+      >
+         <div className="col my-auto  mx-auto">
             {item.complete ? 
             <i 
                onClick={() => props.changeTodoStatus(item)} 
-               className="far fa-check-circle status-icon-done"></i> : 
+               className="far fa-check-circle fa-sm status-icon-done"></i> : 
             <i 
                onClick={() => props.changeTodoStatus(item)} 
-               className="far fa-circle status-icon"></i>}
+               className="far fa-circle fa-sm status-icon"></i>}
          </div>
 
-         <div className="col-9 mt-3">
+         <div className="col-9 mt-3 task mx-auto">
             {item.complete ? <p className="done-task">{item.task}</p> : <p>{item.task}</p>}
          </div>
          
-         <div className="col my-auto text-center">
-            <i 
-            onClick={() => props.deleteFromList(item)} 
-            className="delete far fa-trash-alt" 
-            
-            ></i>
+         <div className="col my-auto mx-auto">
+            {hover ? 
+               <i 
+                  onClick={() => props.deleteFromList(item)} 
+                  className="delete far fa-trash-alt fa-sm" 
+               ></i> : null}
          </div>
          
-      </li>)
+      </li>
+   )
 }
 
 const mapStateToProps = (state /*, ownProps*/) => {
@@ -39,6 +46,4 @@ const mapStateToProps = (state /*, ownProps*/) => {
  }
  const mapDispatchToProps = { addTodo, changeTodoStatus, deleteFromList }
  
- 
  export default connect(mapStateToProps, mapDispatchToProps)(Todo);
- 
